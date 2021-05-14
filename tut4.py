9e18fa0f659191222efe9063c1aad0ff9fa24034
A.Karwowski <akk42@zips.uakron.edu>
2021-05-14 12:42:38 -0400

    revert to previous tut4

--- tut4.py
+++ tut4.py
@@ -8,14 +8,12 @@ while True:
     width = int(cap.get(3))
     height = int(cap.get(4))
     
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_blue = np.array([10,100,100])
-    upper_blue = np.array([179,255,255])
-
-    mask = cv2.inRange(hsv, lower_blue,upper_blue)
-
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.imshow('frame', result)
+    img = cv2.line(frame, (0,0), (width,height),(255,0,0), 10)
+    img = cv2.line(img, (0,height), (width,0),(0,255,0), 10)
+    img = cv2.rectangle(img, (100,100), (200,200), (255,255,255), 5, -1)
+    img = cv2.circle(img, (300,300), 60, (0,0,128) ,-1)
+    img = cv2.putText(img, 'Alex Is Cool!', (200, height-20), font, 2, (233,125,12), 3, cv2.LINE_AA)
+    cv2.imshow('frame', img)
     
     if cv2.waitKey(1) == ord('q'):
         break
