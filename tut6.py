840d5cd3c53e8893877b5e8aee96b24834060926
A.Karwowski <akk42@zips.uakron.edu>
2021-08-26 13:10:21 -0400

    finish tutorial number 6

--- tut6.py
+++ tut6.py
@@ -18,6 +18,8 @@ for i in range(len(corners)):
         color = tuple(map(lambda x:int(x), np.random.randint(0, 255, size=3)))
         cv2.line(img, corner1, corner2, color, 1)
 
+        
+
 
 
 cv2.imshow("checkers", img)
